318fb6c6a549e7f67e756de89434542d9d6a3f6c
bhattanurag <60163583+bhattanurg@users.noreply.github.com>
2020-08-18 02:00:44 +0530

    adding random animation and sound
    which button is clicked

--- game.js
+++ game.js
@@ -1,5 +1,28 @@
-var buttonColours=["red","blue","green","yellow"];
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
 
-function nextSequence(){
-  var num=Math.floor(Math.random()*4);
+
+
+
+
+function nextSequence() {
+  var num = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[num];
+  gamePattern.push(randomChosenColour);
+
+
+
+  var choosenButton = $("#" + randomChosenColour);
+  choosenButton.fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + choosenButton + ".mp3");
+  audio.play();
 }
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+  // console.log(userClickedPattern);
+});
