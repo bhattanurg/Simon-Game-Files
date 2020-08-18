e5eddbb1ed106f13ae8981826cdeed9f82ee778e
bhattanurag <60163583+bhattanurg@users.noreply.github.com>
2020-08-18 13:59:13 +0530

    setting up levels
    changing h1 according to level
    adding sound when clicked
    and also for random click

--- game.js
+++ game.js
@@ -1,12 +1,25 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var userClickedPattern = [];
+  var level=0;
 
+for(var i=0;i<document.querySelectorAll(".btn").length;i++)
+{
+document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+  var userChosenColour = $(this).attr("id");
 
-
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+ console.log(userClickedPattern);
+});
+}
 
 
 function nextSequence() {
+  level++;
+
+    $("h1").text("level " + level);
   var num = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[num];
   gamePattern.push(randomChosenColour);
@@ -16,13 +29,37 @@ function nextSequence() {
   var choosenButton = $("#" + randomChosenColour);
   choosenButton.fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + choosenButton + ".mp3");
+playSound(randomChosenColour);
+animatePress(randomChosenColour);
+
+}
+//
+// $(".btn").click(function(){
+//   var userChosenColour = $(this).attr("id");
+//
+//   userClickedPattern.push(userChosenColour);
+//   // console.log(userClickedPattern);
+// });
+//
+
+
+
+function playSound(name)
+{
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-$(".btn").click(function(){
-  var userChosenColour = $(this).attr("id");
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+              $("#" + currentColour).removeClass("pressed");
 
-  userClickedPattern.push(userChosenColour);
-  // console.log(userClickedPattern);
-});
+      }, 100);
+}
+$("body").keydown(function(event){
+
+  $("h1").text("level " + level);
+  nextSequence();
+
+})
